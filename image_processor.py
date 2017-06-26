51ff82fc1a3e91d9041855de0d485c0fc2704e9d
Sarulian <tedjamesrobinson@gmail.com>
2017-06-26 13:16:54 -0700

    Added compression function

--- image_processor.py
+++ image_processor.py
@@ -14,6 +14,7 @@ def transpose_xy(array):
 
 	return trans_array
 
+
 # returns the given image as a numpy array [x[], y[], color[R,G,B]]
 def get_image_array(filepath):
 
@@ -64,23 +65,26 @@ def swap_colors(image_array):
 	return new_image_array
 
 
-# testing a simple compression (not currently used)
+# reduce number of pixels by 4X
 def compress_image(image_array):
 
-	new_image_array = image_array.copy()
+	print("Compressing image...")
 
-	for j in range(image_array.shape[1]):
-		for i in range(image_array.shape[0]):
-			if i%2 == 0:
-				new_image_array[i,j] = image_array[i,j]
-			else:
-				new_image_array[i,j] = image_array[i-1,j]
+	og_width = image_array.shape[0]
+	og_height = image_array.shape[1]
+	new_image_array = np.zeros( (int(og_width/2), int(og_height/2), 3) , np.uint8)
+
+	for j in range(new_image_array.shape[1]):
+		for i in range(new_image_array.shape[0]):
+			new_image_array[i,j] = average_of([image_array[2*i,2*j], image_array[2*i+1,2*j], image_array[2*i,2*j+1], image_array[2*i+1,2*j+1]])
 
 	return new_image_array
 
 
 # gain resolution by averaging neighboring pixels
 def enlarge_image(image_array):
+
+	print("Enlarging image...")
 
 	og_width = image_array.shape[0]
 	og_height = image_array.shape[1]
@@ -97,23 +101,35 @@ def enlarge_image(image_array):
 		for i in range(new_image_array.shape[0]):
 			if i != 0 and i != new_image_array.shape[0]:
 				if i%2 == 0 and j%2 == 1:
-					new_image_array[i,j] = average_of(new_image_array[i-1,j], new_image_array[i+1,j])
+					new_image_array[i,j] = average_of([new_image_array[i-1,j], new_image_array[i+1,j]])
 
 	# third loop fills the rest of the image with the average of their vertical neighbors
 	for j in range(new_image_array.shape[1]):
 		for i in range(new_image_array.shape[0]):
 			if i != 0 and i != new_image_array.shape[0] and j != 0 and j != new_image_array.shape[1]:
 				if j%2 == 0:
-					new_image_array[i,j] = average_of(new_image_array[i,j+1], new_image_array[i,j-1])
+					new_image_array[i,j] = average_of([new_image_array[i,j+1], new_image_array[i,j-1]])
 
 	return new_image_array
 
 
-# return the average of two color tuples
-def average_of(color1, color2):
+# return the average of a list of color tuples
+def average_of(colors):
 
-	# the int type casting is to prevent an overflow of 8 bit (0-255) ints
-	return ( int((int(color1[0])+int(color2[0]))/2), int((int(color1[1])+int(color2[1]))/2), int((int(color1[2])+int(color2[2]))/2) )
+	red_sum = 0
+	green_sum = 0
+	blue_sum = 0
+
+	for color in colors:
+		red_sum += color[0]
+		green_sum += color[1]
+		blue_sum += color[2]
+
+	red_avg = red_sum/len(colors)
+	green_avg = green_sum/len(colors)
+	blue_avg = blue_sum/len(colors)
+
+	return ( red_avg, green_avg, blue_avg )
 
 
 # main function
@@ -127,7 +143,10 @@ if __name__ == "__main__":
 	filename = sys.argv[1]
 
 	image_array = get_image_array(filename)
-	image_array = enlarge_image(image_array)
+
+	for i in range(50):
+		image_array = compress_image(image_array)
+		image_array = enlarge_image(image_array)
 
 	create_image_from_array(image_array, filename)
 
